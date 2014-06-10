8d124186cb56398c1ad77eebd41f60cb2ba0fabc
Thiago Brandão <thiagobrandam@gmail.com>
2014-06-09 23:15:11 -0300

    Adds Punch image, still a little amateur :|

---
+++ BatePonto/Views/PunchButtonCell.h
@@ -0,0 +1,13 @@
+//
+//  PunchButtonCell.h
+//  BatePonto
+//
+//  Created by thiago on 6/9/14.
+//  Copyright (c) 2014 redealumni. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PunchButtonCell : NSButtonCell
+
+@end
---
+++ BatePonto/Views/PunchButtonCell.m
@@ -0,0 +1,18 @@
+//
+//  PunchButtonCell.m
+//  BatePonto
+//
+//  Created by thiago on 6/9/14.
+//  Copyright (c) 2014 redealumni. All rights reserved.
+//
+
+#import "PunchButtonCell.h"
+
+@implementation PunchButtonCell
+
+- (void)awakeFromNib
+{
+    [super setHighlightsBy:NSChangeGrayCellMask];
+}
+
+@end
